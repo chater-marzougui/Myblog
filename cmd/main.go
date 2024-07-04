488bfb3013deaf2c16440e0768f296f5e23ef201
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-07-04 17:04:13 +0100

    Deleted css Files and Integrated in html

--- cmd/main.go
+++ cmd/main.go
@@ -22,7 +22,6 @@ func main() {
 	http.HandleFunc("/new", handlers.NewPost)
 	http.HandleFunc("/register", handlers.Register)
 
-	http.Handle("/templates/css/", http.StripPrefix("/templates/css/", http.FileServer(http.Dir("/templates/css"))))
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
