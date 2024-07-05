4a7f92ab738679c3748eb7daf4b732f9c67741da
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-07-05 01:43:17 +0100

    Edit Post Management

--- internal/models/post.go
+++ internal/models/post.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"myblog/internal/global"
 	"time"
 )
 
@@ -10,16 +12,17 @@ type Post struct {
 	Title     string
 	Content   string
 	Image     string
+	UserID    int
 	CreatedAt time.Time
 }
 
 func CreatePost(title, content, image string) error {
-	_, err := DB.Exec("INSERT INTO posts (title, content, image) VALUES (?, ?, ?)", title, content, image)
+	_, err := DB.Exec("INSERT INTO posts (title, content, image ,user_ID) VALUES (?, ?, ?, ?)", title, content, image, global.GetUserID())
 	return err
 }
 
 func GetPosts() ([]Post, error) {
-	rows, err := DB.Query("SELECT id, title,image , content, created_at FROM posts")
+	rows, err := DB.Query("SELECT id, title, image, content, created_at FROM posts")
 	if err != nil {
 		return nil, err
 	}
@@ -48,6 +51,18 @@ func GetPost(id int) (*Post, error) {
 	return &post, nil
 }
 
+func GetUser(id int) (*User, error) {
+	row := DB.QueryRow("SELECT ID, Username, Icon, Created_At, email, Password FROM users WHERE id = ?", id)
+	var user User
+	err := row.Scan(&user.ID, &user.Username, &user.Icon, &user.CreatedAt, &user.Email, &user.Password)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
 func UpdatePost(id int, title, content string) error {
 	_, err := DB.Exec("UPDATE posts SET title = ?, content = ? WHERE id = ?", title, content, id)
 	return err
@@ -56,4 +71,17 @@ func UpdatePost(id int, title, content string) error {
 func DeletePost(id int) error {
 	_, err := DB.Exec("DELETE FROM posts WHERE id = ?", id)
 	return err
+}
+
+func UpdateUser(user User) error {
+	query := `
+        UPDATE users
+        SET username = ?, email = ?, password = ?, icon = ?
+        WHERE id = ?
+    `
+	_, err := DB.Exec(query, user.Username, user.Email, user.Password, user.Icon, user.ID)
+	if err != nil {
+		return errors.New("failed to update user: " + err.Error())
+	}
+	return nil
 }
